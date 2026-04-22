4ae5d3b4f5ea6d0bd50f49803f34db0adfb1be92
Emeka Divine <divineemeka.dev@gmail.com>
2026-04-22 16:42:26 +0100

    Add Project type definition in projects.ts
    Verify

---
+++ src/data/projects.ts
@@ -0,0 +1,11 @@
+export type Project = {
+  slug: string
+  title: string
+  description: string
+  fullDescription?: string
+  images: string[]
+  category: string
+  status: string
+  location: string
+  services: string[]
+}
