b787129aab7e206f59d8d0c7204d55dc56a72fde
Emeka Divine <divineemeka.dev@gmail.com>
2026-04-22 16:52:43 +0100

    Update App.tsx
    Adding the Project Details page routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import LandingPage from './pages/LandingPage'
 import Projects from './pages/Projects'
+import ProjectDetailsPage from './pages/ProjectDetailsPage'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route index element={<LandingPage />} />
           <Route path="projects" element={<Projects />} />
+          <Route path="projects/:slug" element={<ProjectDetailsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
